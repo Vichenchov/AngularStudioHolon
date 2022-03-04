2fb72582b77bef81913cd5db093588f0af973f14
Vichenchov <max.viva10@gmail.com>
2022-03-05 01:22:48 +0200

    setup project, generate components and created title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './components/title/title.component';
+import { DrawingComponent } from './components/drawing/drawing.component';
+import { TextComponent } from './components/text/text.component';
+import { BottomComponent } from './components/bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    DrawingComponent,
+    TextComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
